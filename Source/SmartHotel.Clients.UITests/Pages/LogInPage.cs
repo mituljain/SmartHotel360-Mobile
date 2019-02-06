07233f1c2da6a5bf8dd5ec3b04e2af77a35c903a
mituljain <25337693+mituljain@users.noreply.github.com>
2019-02-06 15:28:59 +0530

    Reverted

--- Source/SmartHotel.Clients.UITests/Pages/LogInPage.cs
+++ Source/SmartHotel.Clients.UITests/Pages/LogInPage.cs
@@ -16,15 +16,9 @@ namespace SmartHotel.Clients.UITests.Pages
 
         public LogInPage()
         {
-            App.WaitForElement(x => 
-            x.Property("Text")
-            .Contains("username"),
-            timeout: TimeSpan.FromSeconds(60));
-            
             emailField = x => x.Marked("username");
             passwordField = x => x.Marked("password");
             signInButton = x => x.Marked("signin");
-
         }
 
         public LogInPage EnterCredentials(string username, string password)
